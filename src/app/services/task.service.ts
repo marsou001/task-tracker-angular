5b00c0feb2fc895cc9ac6c744a21dc3008e1c333
Marouane Souda <marsou431@gmail.com>
2022-01-15 18:16:28 +0100

    init commit

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiURL = "http://localhost:5000/tasks";
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiURL)
+  }
+}
